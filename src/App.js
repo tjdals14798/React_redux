62afd074c4e37b6fed25eff0f05690990d8ce2cb
성민 <tjdals14798@naver.com>
2022-08-05 15:40:45 +0900

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Nav, Tab, Row, Col, Badge } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Tab.Container id="left-tabs-example" defaultActiveKey="first" >
+      <Row>
+        <Col md={2} className="justify-content-center">
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <h1 style={{textAlign:"center"}}><Badge bg="dark">Redux</Badge></h1>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">
+                Tab 2
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col md={10}>
+          <Tab.Content>
+            <Tab.Pane eventKey="first">
+              
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+            안녕하세요
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+    </>
   );
 }
 
