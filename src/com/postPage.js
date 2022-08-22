ff6d77d4e5f11a133c8f537191affa41c34ce970
성민 <tjdals14798@naver.com>
2022-08-22 20:05:49 +0900

    22-08-22

--- src/com/postPage.js
+++ src/com/postPage.js
@@ -12,7 +12,7 @@ const PostItem = React.memo(function TodoItem({ post, setModalShow, value, setIn
   return (
     <>
       <td>{post.id}</td>
-      <td><Button variant="none" size="sm" className="w-100" index={value} onClick={onModal}> {post.postname} </Button></td>
+      <td><Button variant="link" size="sm" className="w-100" index={value} onClick={onModal} style={{textDecoration:"none", color:"white"}}> {post.postname} </Button></td>
     </>
   );
 });
@@ -38,10 +38,8 @@ function PostModal({ post, show, setModalShow, postIndex }){
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>{post[postIndex].postname}</h4>
-        <p>
-          {post[postIndex].postcontent}
-        </p>
+        <h4> {post[postIndex].postname} </h4>
+        <p> {post[postIndex].postcontent} </p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={()=>setModalShow(false)}> 닫기 </Button>
@@ -50,8 +48,28 @@ function PostModal({ post, show, setModalShow, postIndex }){
   )
 }
 
+function PostingModal({ show,setPostModal }){
+  return(
+  <Modal size="lg" centered show={show}>
+      <Modal.Header>
+        <Modal.Title id="contained-modal-title-vcenter">
+          글쓰기
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4></h4>
+        <p> </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={()=>setPostModal(false)}> 닫기 </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
 function PostPage({ post, postIndex, setIndex }){
   const [modalShow,setModalShow] = useState(false);
+  const [postModal, setPostModal] = useState(false);
     return(
       <>
       <Container>
@@ -63,10 +81,12 @@ function PostPage({ post, postIndex, setIndex }){
           </thead>
           <tbody>
             <tr><th>작성자</th><th>글 제목</th></tr>
-            <PostList posts={post} setModalShow={setModalShow} setIndex={setIndex}/>
+            <PostList posts={post} setModalShow={setModalShow} setIndex={setIndex}/>  
+            <tr><td colSpan={2}><Button size="sm" variant="outline-light" onClick={()=>setPostModal(true)} > 글쓰기 </Button></td></tr>
           </tbody>
         </Table>
         <PostModal show={modalShow} post={post} postIndex={postIndex} setModalShow={setModalShow}/>
+        <PostingModal show={postModal} setPostModal={setPostModal}/>
       </Container>
       </>
     )
