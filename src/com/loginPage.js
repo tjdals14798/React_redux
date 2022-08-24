8e07b4598f1050b9d15fec577739d7076d4549fb
성민 <tjdals14798@naver.com>
2022-08-24 14:23:10 +0900

    22-08-24

--- src/com/loginPage.js
+++ src/com/loginPage.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Container, Form, Modal, Table, Toast, ToastContainer } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function LoginTost({cklogin ,onLogOut, userName, setLoginModal}){
+function LoginTost({cklogin ,onLogOut, userName, setLoginModal, setChangeModal }){
   return(
     <>
     {!cklogin &&
@@ -13,13 +13,13 @@ function LoginTost({cklogin ,onLogOut, userName, setLoginModal}){
             <small>^ _ ^</small>
           </Toast.Header>
           <Toast.Body>
-            {userName} 님 환영합니다!!<hr/>
+            로그인을 해주세요
+            <hr/>
             <Button variant="outline-secondary" size="sm" onClick={()=>setLoginModal(true)}>로그인</Button>
           </Toast.Body>
         </Toast>
       </ToastContainer>
-      }
-
+    }
     {cklogin &&
        <ToastContainer className="p-3" position="top-end">
       <Toast>
@@ -29,6 +29,7 @@ function LoginTost({cklogin ,onLogOut, userName, setLoginModal}){
         </Toast.Header>
         <Toast.Body>
           {userName} 님 환영합니다!!<hr/>
+          <Button variant="outline-secondary" size="sm" onClick={()=>setChangeModal(true)}>정보변경</Button>
           <Button variant="outline-secondary" size="sm" onClick={onLogOut}>로그아웃</Button>
         </Toast.Body>
       </Toast>
@@ -38,7 +39,7 @@ function LoginTost({cklogin ,onLogOut, userName, setLoginModal}){
   )
 }
 
-function LoginModal({onSearch, loginModal, setLoginModal}){
+function LoginModal({ onSearch, loginModal, setLoginModal }){
   const [Inputs,setInputs] = useState({
     id: '',
     password: ''
@@ -58,6 +59,7 @@ function LoginModal({onSearch, loginModal, setLoginModal}){
       id: '',
       password: ''
     });
+    setLoginModal(false);
   }
 
   return(
@@ -65,20 +67,10 @@ function LoginModal({onSearch, loginModal, setLoginModal}){
     <Modal size="lg" centered show={loginModal}>
       <Modal.Header>
         <Modal.Title id="contained-modal-title-vcenter">
-          글쓰기
+          로그인
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-      <Table striped bordered hover variant="dark" className="mt-4 w-75">
-        <thead>
-          <tr>
-            <th colSpan={2}>로그인</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-            <Container fluid="xl">
             <Form>
               <Form.Group required controlId="formBasicEmail">
                 <Form.Control type="text" autoFocus placeholder="ID 를 입력하세요" required name="id" value={id} onChange={loginOnChange}/>
@@ -86,11 +78,6 @@ function LoginModal({onSearch, loginModal, setLoginModal}){
               </Form.Group>
               <Button variant="secondary" className="mt-2 w-100" onClick={loginOnSubmit}> 로그인 </Button>
             </Form>
-            </Container>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={()=>setLoginModal(false)}> 닫기 </Button>
@@ -100,57 +87,66 @@ function LoginModal({onSearch, loginModal, setLoginModal}){
   )
 }
 
-function Loginpage ({ onSearch, cklogin, userName, onLogOut, onNChange }){
-  const [loginModal,setLoginModal] = useState(false);
-  // const [Inputs,setInputs] = useState({
-    //     id: '',
-    //     password: ''
-    // });
-    // const { id, password } = Inputs;
-    
-    const [changeInputs,setChangeInputs] = useState({
+function ChangeInfoModal({ changeModal, setChangeModal, onNChange, userName }){
+  console.log(userName);
+  const [changeInputs,setChangeInputs] = useState({
+    changeid: '',
+    changenickname: ''
+  });
+  const { changeid, changenickname } = changeInputs;
+
+  const NickonChange = e => {
+    const {name, value} = e.target;
+    setChangeInputs({
+      ...changeInputs,
+      [name]:value
+    });
+  }
+
+  const NickonSubmit = () => {
+    onNChange(changeInputs);
+    setChangeInputs({
       changeid: '',
       changenickname: ''
     });
-    const { changeid, changenickname } = changeInputs;
+    setChangeModal(false);
+  }
 
-    // const loginOnChange = e => {
-    //   const {name, value} = e.target;
-    //   setInputs({
-    //     ...Inputs,
-    //     [name]:value
-    //   });
-    // }
-      
-    // const loginOnSubmit = () => {
-    //   onSearch(Inputs);
-    //   setInputs({
-    //     id: '',
-    //     password: ''
-    //   });
-    // }
+  return(
+    <>
+      <Modal size="lg" centered show={changeModal}>
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+          닉네임 변경
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group required controlId="formBasicEmail">
+              <Form.Control type="text" placeholder="ID 를 입력하세요" name="changeid" value={changeid} onChange={NickonChange}/>
+              <Form.Control type="text" placeholder="NICKNAME 를 입력하세요" required className="my-1" name="changenickname" value={changenickname} onChange={NickonChange}/>
+            </Form.Group>
+            <Button variant="secondary" className="mt-2 w-100" onClick={NickonSubmit}> 변경 </Button>
+          </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={()=>{setChangeModal(false)}}> 닫기 </Button>
+      </Modal.Footer>
+    </Modal>
+    </>
+  )
+}
 
-    const NickonChange = e => {
-      const {name, value} = e.target;
-      setChangeInputs({
-        ...changeInputs,
-        [name]:value
-      });
-    }
-  
-    const NickonSubmit = () => {
-      onNChange(changeInputs);
-      setChangeInputs({
-        changeid: '',
-        changenickname: ''
-      });
-    }
-    
+function Loginpage ({ onSearch, cklogin, userName, onLogOut, onNChange }){
+  const [loginModal,setLoginModal] = useState(false);
+  const [changeModal,setChangeModal] = useState(false);
+
     return(
-        <>
+      <>
         <Container>
-          <LoginTost loginModal={loginModal} setLoginModal={setLoginModal}/>
-          <LoginModal loginModal={loginModal}/>
+          <LoginTost loginModal={loginModal} setLoginModal={setLoginModal} setChangeModal={setChangeModal} onLogOut={onLogOut} userName={userName} cklogin={cklogin}/>
+          <LoginModal loginModal={loginModal} setLoginModal={setLoginModal} onSearch={onSearch} userName={userName} cklogin={cklogin}/>
+          <ChangeInfoModal changeModal={changeModal} setChangeModal={setChangeModal} onNChange={onNChange}/>
         {/* {!cklogin &&
           <Table striped bordered hover variant="dark" className="mt-4 w-75">
             <thead>
@@ -175,7 +171,7 @@ function Loginpage ({ onSearch, cklogin, userName, onLogOut, onNChange }){
             </tbody>
           </Table>
         } */}
-        {cklogin &&
+        {/* {cklogin &&
           <>
           <Table striped bordered hover variant="dark" className="mt-4 w-75">
             <thead>
@@ -201,9 +197,9 @@ function Loginpage ({ onSearch, cklogin, userName, onLogOut, onNChange }){
           </Table>
           <LoginTost userName={userName} onLogOut={onLogOut} />
           </>
-        }
+        } */}
         </Container>
-        </>
+      </>
     )
 }
 
