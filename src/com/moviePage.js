40923181f410001b65b9f620dceede0edece13f8
성민 <tjdals14798@naver.com>
2022-09-20 22:17:14 +0900

    22-09-20

--- src/com/moviePage.js
+++ src/com/moviePage.js
@@ -6,57 +6,43 @@ import { ChevronRight, ChevronLeft } from 'react-bootstrap-icons';
 
 function Heder(){
     return(
-        <>
+        <div className="contentDiv">
             <h1 style={{textAlign:"left"}}>시집이</h1>
             <hr id="movie_heder"/>
-            <Navbar collapseOnSelect className="w-75" expand="lg">
+            <Navbar collapseOnSelect className="w-100" expand="lg">
                 <Container style={{cursor:'pointer',color:"white"}}>                         
                     <Nav><li className="movie_li">영화</li><li className="movie_li">극장</li><li className="movie_li">예매</li><li className="movie_li">스토어</li><li className="movie_li">이벤트</li></Nav>
                 </Container>
             </Navbar>
-            <img className="d-block w-75" style={{ height:"500px",borderTop:"2px solid red",width:"100%"}} src={require("../codeimg/dragon.jpg")}/>
-        <style>
-        {`
-            .movie_li{
-                display:block;
-                float: left;
-                list-style:none;
-                padding-left:40px;
-                color:black;
-                height:30px;
-            }
-            #movie_heder{
-                margin:0px;
-                width:75%;
-            }
-        `}
-        </style>
-        </>
+            <img className="d-block w-100" style={{ height:"500px",borderTop:"2px solid red"}} src={require("../codeimg/dragon.jpg")}/>
+        </div>
     )
 }
 
-function Movie({ movies, setCurrentPage, currentPage }){
+function Movie({movies, setCurrentPage, currentPage }){
     return(
-        <>
+        <div className="contentDiv">
         <Container fluid>
             <h1 className="mt-2" style={{textAlign:"left"}}>무비 차트</h1>
-            <Button size="lg" variant="secondary" onClick={()=>setCurrentPage(currentPage-1)} style={{position: 'relative', zIndex:"10", left: "-85%", top:120 ,borderRadius:"50px"}}><h3><ChevronLeft/></h3></Button>
             {movies.map((movie,i) =>(
-                <li key={i} style={{float: "left", listStyle:"none", marginLeft:"7px"}}>
-                 <Card style={{ width: '14rem', height:"330px" }}>
-                 <Card.Img variant="top" src={movie.Poster} style={{height:"200px"}}/>
+                <li key={i} style={{position: 'sticky', float: "left", listStyle:"none", marginLeft:"7px"}}>
+                 <Card style={{ width: '14rem', height:"300px" }}>
+                 <Card.Img variant="top" src={require("../codeimg/noimg.gif")} style={{height:"200px"}}/>
                  <Card.Body>
-                   <Card.Title>{movie.Title}</Card.Title>
+                   <Card.Title style={{ overflow:"hidden", textOverflow: "ellipsis", whiteSpace: "nowrap"}}>{movie.movieNm}</Card.Title>
                    <Card.Text>
-                     개봉연도:{movie.Year}
+                     개봉일:{movie.openDt}
                    </Card.Text>
                  </Card.Body>
                </Card>
                </li>
             ))}
-            <Button size="lg" variant="secondary" onClick={()=>setCurrentPage(currentPage+1)} style={{position: 'relative', zIndex:"10", right: "15%", top:120 ,borderRadius:"50px"}}><h3><ChevronRight/></h3></Button>
-            </Container>
-        </>
+            <div className="contentDiv" style={{position:"relative", minWidth:"1200px"}}>
+            <Button variant="secondary" onClick={()=>setCurrentPage(currentPage-1)} style={{position: 'absolute', zIndex:"10", left: "-1.5%", top:170 ,borderRadius:"50px"}}><h3><ChevronLeft/></h3></Button>
+            <Button variant="secondary" onClick={()=>setCurrentPage(currentPage+1)} style={{position: 'absolute', zIndex:"10", right: "1.5%", top:170 ,borderRadius:"50px"}}><h3><ChevronRight/></h3></Button>
+            </div>
+        </Container>
+        </div>
     )
 }
 
@@ -73,9 +59,9 @@ export default function MoviePage(){
             // loding를 true로 변경
             setLoding(true);
             const response = await axios.get(
-                'http://www.omdbapi.com/?apikey=342d473&s=ironman'
+                'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101'
             );
-            setMovie(response.data.Search);
+            setMovie(response.data.boxOfficeResult.dailyBoxOfficeList);
         } catch (e){
             setError(e);
         }
@@ -85,7 +71,7 @@ export default function MoviePage(){
     useEffect(()=>{
         JSON.stringify(fetchUsers());
     },[]);
-
+    console.log(movie)
     const [currentPage, setCurrentPage] = useState(1);
     const postsPerPage = 5;
     const indexOfLast = currentPage * postsPerPage;
@@ -102,15 +88,28 @@ export default function MoviePage(){
     if(!movie) return null;
 
     return(
-        <>
+        <div style={{position:"relative", width:"100%", minWidth:"1280px"}}>
             <Heder/>
-            <Movie movies={currentPosts(movie)} setCurrentPage={setCurrentPage} currentPage={currentPage}/>
+            <Movie movies={currentPosts(movie)} setCurrentPage={setCurrentPage} currentPage={currentPage} />
 
             <style>
-                {`
-                    width:100;
-                `}
-            </style>
-        </>
+        {`
+            .movie_li{
+                display:block;
+                float: left;
+                list-style:none;
+                padding-left:40px;
+                color:black;
+                height:30px;
+            }
+            #movie_heder{
+                margin:0px;
+            }
+            .contentDiv{
+                width:1200px;
+            }
+        `}
+        </style>
+        </div>
     )
 }
